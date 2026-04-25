55b37a2c2e8a8adba2ce2724fdad484fdd8d7b89
GaspardG0001 <gaspard.grandidier@netcourrier.com>
2026-04-26 00:05:18 +0200

    Update developer accounts to include additional usernames

--- electron/const.ts
+++ electron/const.ts
@@ -1,2 +1,2 @@
 export const ADMINTOOL_URL = 'https://eml.mcflowblock.com/'
-export const DEVELOPPER_ACCOUNTS = ['gaspardg']
+export const DEVELOPPER_ACCOUNTS = ['gaspardg', 'fatalerror6126', 'kounac2702']
